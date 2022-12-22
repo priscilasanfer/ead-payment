4b1da720efad06a07a9e671dcd546a622e680c1d
Priscila Ferreira <priscilasanfer@gmail.com>
2022-12-21 21:51:11 -0300

    Event State Transfer Pattern no Payment Microservice

---
+++ src/main/java/com/ead/payment/models/UserModel.java
@@ -0,0 +1,100 @@
+package com.ead.payment.models;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import java.io.Serializable;
+import java.util.Set;
+import java.util.UUID;
+
+
+@Entity
+@Table(name = "TB_USERS")
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class UserModel implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    private UUID userId;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String email;
+
+    @Column(nullable = false, length = 150)
+    private String fullName;
+
+    @Column(nullable = false)
+    private String userStatus;
+
+    @Column(nullable = false)
+    private String userType;
+
+    @Column(length = 20)
+    private String cpf;
+
+    @Column(length = 20)
+    private String phone_number;
+
+    public UUID getUserId() {
+        return userId;
+    }
+
+    public void setUserId(UUID userId) {
+        this.userId = userId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getUserStatus() {
+        return userStatus;
+    }
+
+    public void setUserStatus(String userStatus) {
+        this.userStatus = userStatus;
+    }
+
+    public String getUserType() {
+        return userType;
+    }
+
+    public void setUserType(String userType) {
+        this.userType = userType;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getPhone_number() {
+        return phone_number;
+    }
+
+    public void setPhone_number(String phone_number) {
+        this.phone_number = phone_number;
+    }
+}
